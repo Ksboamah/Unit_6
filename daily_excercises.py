0c0280b67b71d16699b53d8c1be63f37f6816b7c
Kwame <bkwamesakyi@gmail.com>
2019-10-28 11:21:20 -0400

    Initial commit

---
+++ daily_excercises.py
@@ -0,0 +1,21 @@
+# names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen", "Jackie", "Kurt",
+#          "Linda"]
+# print(names[3:5])
+# print(names[1:6])
+# print(names[5:13])
+# print(names[5:])
+# print(names[11])
+# print(names[-1])
+# print(names[1:8:2])
+# print(names[::2])
+# print(names[0:12:2])
+# print(names[12:8:-1])
+# print(names[::-1])
+# print(names[12::-1])
+
+
+def are_duplicates(nums):
+    if x > 1
+        return True
+    else:
+        return False
