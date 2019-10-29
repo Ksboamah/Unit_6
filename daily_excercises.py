9b2c03dea1beffac79c15d6b9e0aa2a8b738bc21
Kwame <bkwamesakyi@gmail.com>
2019-10-29 12:06:02 -0400

    I started my Unit 6 daily exercises. I worked on my Unit 6 daily exercises.

--- daily_excercises.py
+++ daily_excercises.py
@@ -1,3 +1,5 @@
+# By Kwame Boamah
+
 # names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen", "Jackie", "Kurt",
 #          "Linda"]
 # print(names[3:5])
@@ -15,7 +17,22 @@
 
 
 def are_duplicates(nums):
-    if x > 1
-        return True
-    else:
-        return False
+    for x in range(len(nums)):
+        for y in range(x + 1, len(nums)):
+            if nums[x] == nums[y]:
+                return True
+    return False
+
+
+def add_numbers(nums):
+    total = 0
+    for x in nums:
+        total += x
+
+
+def main():
+    print(are_duplicates([1, 2, 3, 4, 5, 6, 3, 7]))
+    print(add_numbers([1, 2, 3, 6, 6, 7]))
+
+
+main()
